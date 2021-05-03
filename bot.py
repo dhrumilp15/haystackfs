8622b09e4b4fbfb1e5a3be265368513d7f6a8ec5
Dhrumil Patel <dhrumilp15@gmail.com>
2021-05-02 22:25:44 -0400

    Working on Discord Server

--- bot.py
+++ bot.py
@@ -1,9 +1,12 @@
 import os
+from pathlib import Path
+import requests
+from io import BytesIO
+
 import discord
 from dotenv import load_dotenv
-from pathlib import Path
-import boto3
-from upload_to_s3 import upload_to_s3
+# import boto3
+# from upload_to_s3 import upload_to_s3
 from elasticsearch_conn import ElasticSearchConnector
 
 env_path = Path('.') / '.env'
@@ -14,13 +17,11 @@ TOKEN = os.getenv('DISCORD_TOKEN')
 
 client = discord.Client()
 
-s3_client = boto3.client(
-    's3', endpoint_url=str(os.getenv('ENDPOINT_URL')),
-    aws_access_key_id=str(os.getenv('AWS_ACCESS_KEY_ID')),
-    aws_secret_access_key=str(os.getenv('AWS_SECRET_ACCESS_KEY'))
-)
-
-print(s3_client.list_buckets())
+# s3_client = boto3.client(
+#     's3', endpoint_url=str(os.getenv('ENDPOINT_URL')),
+#     aws_access_key_id=str(os.getenv('AWS_ACCESS_KEY_ID')),
+#     aws_secret_access_key=str(os.getenv('AWS_SECRET_ACCESS_KEY'))
+# )
 
 es_client = ElasticSearchConnector(
     elastic_domain=os.getenv("ELASTIC_DOMAIN"),
@@ -39,6 +40,37 @@ async def on_message(message):
     '''Send all message attachments to the CORTX s3 bucket'''
     if message.author == client.user:
         return
-    await upload_to_s3(s3_client, es_client, message)
+
+    # since cortx has been terminated, we won't be using it anymore :(
+    es_client.create_doc(message)
+
+    if message.content[:7] == '!search' or message.content[:2] == '!s':
+        content = ''.join(message.content.split()[1:])
+        files = es_client.search(content)
+        await message.channel.send("Found these:")
+
+        file_buf = await download(files)
+        print(file_buf)
+        await message.channel.send(files=file_buf)
+        for buf in file_buf:
+            buf.close()
+
+
+async def download(files: str):
+    filebufs = []
+    for file in files:
+        url = file['_source']['url']
+        response = requests.get(url, stream=True)
+        if not response.ok:
+            print(response)
+        file_buf = BytesIO()
+        for blk in response.iter_content(1024):
+            if not blk:
+                break
+            file_buf.write(blk)
+        file_buf.seek(0)
+        filebufs.append(discord.File(file_buf, file['_source']['url']))
+    return filebufs
+
 
 client.run(TOKEN)
--- elasticsearch_conn.py
+++ elasticsearch_conn.py
@@ -5,66 +5,89 @@ from elasticsearch.exceptions import ConflictError
 
 class ElasticSearchConnector():
     def __init__(self, elastic_domain: str, elastic_port: str, index: str = 'file_index'):
-        self.es = self.make_connection(elastic_domain, elastic_port)
+        self.ES = self.make_connection(elastic_domain, elastic_port)
         self.index = index
         self.create_index()
 
     def make_connection(self, domain: str, port: str):
         try:
-            es = Elasticsearch(domain + ':' + port)
-            print(json.dumps(Elasticsearch.info(es), indent=4))
-            return es
+            return Elasticsearch(domain + ':' + port)
         except Exception as err:
             print(f"Encountered {err}")
             return None
 
     def create_index(self):
-        if not self.es.indices.exists(index=self.index):
-            self.es.indices.create(index=self.index, body=json.load(
-                open('elasticsearchconfig.json', 'r')))
+        self.ES.indices.delete(index=self.index, ignore=[400, 404])
+        self.ES.indices.create(index=self.index, body=json.load(
+            open('elasticsearchconfig.json', 'r')))
 
-    def check_if_doc_exists(self, file_name: str):
-        return self.es.exists(index=self.index, id=file_name)
+    def check_if_doc_exists(self, filename):
+        return self.ES.exists(index=self.index, id=filename)
 
-    def create_doc(self, file, message):
-        if self.check_if_doc_exists(file.filename):
-            self.delete_doc(file.filename)
-        try:
-            body = {
-                "author": str(message.author.id),
-                "author_name": message.author.name,
-                "file_id": str(file.id),
-                "file_name": file.filename,
-                "created": str(message.created_at),
-                "mimetype": str(file.content_type),
-                "size": str(file.size),
-                "proxy_url": str(file.proxy_url),
-                "url": str(file.url)
-            }
-            if file.height and file.width:
-                body["height"] = file.height
-                body["width"] = file.width
+    def create_doc(self, message):
+        for file in message.attachments:
+            print("Attempting to create {}, hash: {}".format(
+                file.filename, hash(file)))
 
-            self.es.create(index=self.index, id=file.filename, body=body)
-            return True
-        except ConflictError as err:
-            print(f"Error is {err}")
-            return False
+            try:
+                # filename = self.update_duplicate_filename(file.filename)
+                body = {
+                    "author": str(message.author.id),
+                    "author_name": message.author.name,
+                    "file_id": str(file.id),
+                    "file_name": file.filename,
+                    "created": str(message.created_at),
+                    "mimetype": str(file.content_type),
+                    "size": str(file.size),
+                    "proxy_url": str(file.proxy_url),
+                    "url": str(file.url)
+                }
+                if file.height and file.width:
+                    body["height"] = file.height
+                    body["width"] = file.width
+                self.ES.create(index=self.index, id=file.id, body=body)
+                return True
+            except ConflictError as err:
+                print(f"Error is {err}")
+                return False
 
-    def delete_doc(self, file_name):
+    def delete_doc(self, filename):
         """Removes a document from the index"""
-        if self.check_if_doc_exists(file_name):
-            self.es.delete(index=self.index, id=file_name)
+        if self.check_if_doc_exists(filename):
+            self.ES.delete(index=self.index, id=filename)
 
-    def search(self, text: str):
-        query = {"from": 0, "size": 20, "query": {
-            "match": {"file_name": {"query": text, "operator": "and"}}}}
-        res = self.es.search(index=self.index, body=query)
-        return res["hits"]["hits"]
+    def update_duplicate_filename(self, filename):
+        existing_filenames = self.search(filename)
+        if not existing_filenames:
+            return filename
+
+        prev_word = existing_filenames[-1]['_source']['file_name']
+        for idx, char in enumerate(prev_word[::-1]):
+            if char == '_':
+                version = int(prev_word[-idx + 1:])
+                filename += '_' + str(version + 1)
+                return filename
+            if char == '.':
+                break
+
+        return filename + '_1'
+
+    def search(self, filename: str):
+        query = {
+            "query": {
+                "match": {
+                    "file_name": {
+                        "query": filename,
+                        "fuzziness": "AUTO",
+                    }
+                }
+            }
+        }
+        return self.ES.search(index=self.index, body=query)["hits"]["hits"]
 
     def get_all_docs(self):
         """Get all docs in ES"""
-        result = self.es.search(
+        result = self.ES.search(
             index=self.index,
             body={
                 "query": {
@@ -72,9 +95,8 @@ class ElasticSearchConnector():
                 }
             }
         )
-        # print(result["hits"]["hits"])
-        return result
+        return result["hits"]["hits"]
 
 
 if __name__ == '__main__':
-    es = ElasticSearchConnector('http://localhost', '9200', 'file_index')
+    ElasticSearchConnector('http://localhost', '9200', 'file_index')
