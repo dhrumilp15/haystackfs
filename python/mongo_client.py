115cb39fa791790a13f1043cf11e439c9a17dafe
Dhrumil Patel <dhrumilp15@gmail.com>
2021-10-11 22:49:56 -0400

    Mongodb connection is optional

--- python/mongo_client.py
+++ python/mongo_client.py
@@ -3,7 +3,7 @@ import logging
 from motor.motor_asyncio import AsyncIOMotorClient
 from datetime import datetime
 import discord
-from typing import List, Dict
+from typing import List, Dict, Tuple
 import json
 
 from config import CONFIG
@@ -26,13 +26,10 @@ class MgClient:
     def __init__(self, mongo_endpoint: str = None, db_name: str = None):
         """Initialize the MongoDB Client and database."""
         self.client, self.db = None, None
-        try:
-            if mongo_endpoint and db_name:
-                self.client = AsyncIOMotorClient(mongo_endpoint)
-                self.db = self.client[db_name]
-                logger.info(f"Connected to MongoDB! Current database: {self.db.name}")
-        except:
-            logger.info("Couldn't connect to MongoDB")
+        if mongo_endpoint and db_name:
+            self.client = AsyncIOMotorClient(mongo_endpoint)
+            self.db = self.client[db_name]
+            logger.info(f"Connected to MongoDB! Current database: {self.db.name}")
 
     async def get_file(self, file_id: int) -> dict:
         """
@@ -61,13 +58,17 @@ class MgClient:
         for key, val in kwargs.items():
             kwargs[key] = repr(val)
         command_info = utils.command_to_mongo_dict(command_type, ctx, kwargs)
-        command_coll = self.db.commands
-        res = await command_coll.insert_one(command_info)
-        if res.acknowledged:
-            logger.info(f"Inserted new command: {res.inserted_id}")
-        else:
-            logger.error(f"Failed to insert new command {ctx.id}")
-        return res.acknowledged
+        try:
+            command_coll = self.db.commands
+            res = await command_coll.insert_one(command_info)
+            if res.acknowledged:
+                logger.info(f"Inserted new command: {res.inserted_id}")
+            else:
+                logger.error(f"Failed to insert new command {ctx.id}")
+            return res.acknowledged
+        except Exception as e:
+            print(e)
+            return False
 
     async def add_server(self, server: discord.Guild or discord.DMChannel) -> bool:
         """
@@ -83,16 +84,19 @@ class MgClient:
             return False
         server_coll = self.db.servers
         # We've already added the server
-        num_docs = await server_coll.count_documents({"_id": server.id}, limit=1)
-        if num_docs:
-            return True
         server_info = utils.server_to_mongo_dict(server)
-        res = await server_coll.insert_one(server_info)
-        if res.acknowledged:
-            logger.info(f"Inserted new server: {res.inserted_id} with server id: {server.id}")
-        else:
-            logger.error(f"Failed to insert new server with server _id: {server.id}")
-        return res.acknowledged
+        try:
+            num_docs = await server_coll.count_documents({"_id": server.id}, limit=1)
+            if num_docs:
+                return True
+            res = await server_coll.insert_one(server_info)
+            if res.acknowledged:
+                logger.info(f"Inserted new server: {res.inserted_id} with server id: {server.id}")
+            else:
+                logger.error(f"Failed to insert new server with server _id: {server.id}")
+            return res.acknowledged
+        except:
+            return False
 
     async def remove_server(self, server_id: int) -> bool:
         """
@@ -107,12 +111,15 @@ class MgClient:
         if not self.db:
             return False
         server_coll = self.db.servers
-        res = await server_coll.update_one({"_id": server_id}, {'$set': {"bot_in_server": False}})
-        if res.acknowledged:
-            logger.info(f"Marked the bot as not in server {server_id} in {res.modified_count} docs")
-        else:
-            logger.error(f"Failed to mark the bot as not in server {server_id}")
-        return res.acknowledged
+        try:
+            res = await server_coll.update_one({"_id": server_id}, {'$set': {"bot_in_server": False}})
+            if res.acknowledged:
+                logger.info(f"Marked the bot as not in server {server_id} in {res.modified_count} docs")
+            else:
+                logger.error(f"Failed to mark the bot as not in server {server_id}")
+            return res.acknowledged
+        except:
+            return False
 
     async def remove_server_docs(self, server_id: int) -> bool:
         """
@@ -127,8 +134,11 @@ class MgClient:
         if not self.db:
             return False
         files_coll = self.db.files
-        res = await files_coll.delete_many({"guild_id": server_id})
-        return res.acknowledged
+        try:
+            res = await files_coll.delete_many({"guild_id": server_id})
+            return res.acknowledged
+        except:
+            return False
 
     async def add_file(self, message: discord.Message) -> int:
         """
@@ -144,24 +154,26 @@ class MgClient:
             return 0
         files_coll = self.db.files
         files_added = 0
-        # We've already added the files in this message id
-        num_docs = await files_coll.count_documents({"message_id": message.id}, limit=1)
-        if num_docs:
-            return True
-
-        for file in message.attachments:
-            # We've already added this file
-            n_doc = await files_coll.count_documents({"_id": file.id}, limit=1)
-            if n_doc:
-                continue
-            file_info = utils.attachment_to_mongo_dict(message, file)
-            res = await files_coll.insert_one(file_info)
-            if res.acknowledged:
-                logger.info(f"Inserted new file: {res.inserted_id} with file id: {file.id}")
-                files_added += 1
-            else:
-                logger.error(f"Failed to insert file with _id: {file.id}")
-        return files_added
+        try:
+            # We've already added the files in this message id
+            num_docs = await files_coll.count_documents({"message_id": message.id}, limit=1)
+            if num_docs:
+                return True
+            for file in message.attachments:
+                # We've already added this file
+                n_doc = await files_coll.count_documents({"_id": file.id}, limit=1)
+                if n_doc:
+                    continue
+                file_info = utils.attachment_to_mongo_dict(message, file)
+                res = await files_coll.insert_one(file_info)
+                if res.acknowledged:
+                    logger.info(f"Inserted new file: {res.inserted_id} with file id: {file.id}")
+                    files_added += 1
+                else:
+                    logger.error(f"Failed to insert file with _id: {file.id}")
+            return files_added
+        except:
+            return 0
 
     async def remove_file(self, ids: List[str], field: str = "_id") -> bool:
         """
@@ -177,8 +189,11 @@ class MgClient:
         if not self.db:
             return False
         files_coll = self.db.files
-        res = await files_coll.delete_many({field: {"$in": ids}})
-        return res.acknowledged
+        try:
+            res = await files_coll.delete_many({field: {"$in": ids}})
+            return res.acknowledged
+        except:
+            return False
 
     async def dump_snapshot(self, collection):
         """
@@ -188,14 +203,17 @@ class MgClient:
             collection: A collection from mongodb.
         """
         if not self.db:
-            return
-        path = f"{CONFIG.DB_NAME}_{collection.name}"
-        if not os.path.exists(path):
-            os.mkdir(path)
-        coll = {collection.name: [file async for file in collection.find()]}
-        print(f'Snapshot of {CONFIG.DB_NAME}/{collection.name} at {datetime.now().strftime("%Y-%m-%dT%H:%M:%S")}')
-        with open(f"{path}/snapshot_" + datetime.now().strftime("%Y-%m-%dT%H:%M:%S") + ".json", 'w') as f:
-            json.dump(coll, fp=f, default=json_util.default, indent=4)
+            return False
+        try:
+            path = f"{CONFIG.DB_NAME}_{collection.name}"
+            if not os.path.exists(path):
+                os.mkdir(path)
+            coll = {collection.name: [file async for file in collection.find()]}
+            print(f'Snapshot of {CONFIG.DB_NAME}/{collection.name} at {datetime.now().strftime("%Y-%m-%dT%H:%M:%S")}')
+            with open(f"{path}/snapshot_" + datetime.now().strftime("%Y-%m-%dT%H:%M:%S") + ".json", 'w') as f:
+                json.dump(coll, fp=f, default=json_util.default, indent=4)
+        except:
+            pass
 
     async def load_from_snapshot(self, snapshot_path: str) -> bool:
         """
@@ -210,10 +228,13 @@ class MgClient:
         if not self.db:
             return False
         ack = False
-        with open(snapshot_path, 'r') as f:
-            docs = json.load(f, object_hook=json_util.object_hook)
-            res = await self.db.files.insert_many(docs['files'])
-            ack = res.acknowledged
+        try:
+            with open(snapshot_path, 'r') as f:
+                docs = json.load(f, object_hook=json_util.object_hook)
+                res = await self.db.files.insert_many(docs['files'])
+                ack = res.acknowledged
+        except:
+            pass
         return ack
 
     async def mass_remove_file(self, serv_id: str) -> bool:
@@ -229,14 +250,17 @@ class MgClient:
         if not self.db:
             return False
         files_coll = self.db.files
-        res = await files_coll.delete_many({"guild_id": serv_id})
-        if res.acknowledged:
-            logger.info(f"Deleted {res.deleted_count} files with guild_id: {serv_id}")
-        else:
-            logger.error(f"Failed to delete files with guild_id: {serv_id}")
-        return res.acknowledged
+        try:
+            res = await files_coll.delete_many({"guild_id": serv_id})
+            if res.acknowledged:
+                logger.info(f"Deleted {res.deleted_count} files with guild_id: {serv_id}")
+            else:
+                logger.error(f"Failed to delete files with guild_id: {serv_id}")
+            return res.acknowledged
+        except:
+            return False
 
-    async def delete_files_from_inactive_servers(self) -> bool:
+    async def delete_files_from_inactive_servers(self) -> Tuple[bool, bool]:
         """
         Delete files that belong to servers that the bot is in.
 
@@ -246,18 +270,21 @@ class MgClient:
         if not self.db:
             return False
         server_coll = self.db.servers
-        inactive_servers = server_coll.find({"bot_in_server": False})
-        inactive_serv = [serv['_id'] async for serv in inactive_servers]
-        files_coll = self.db.files
-        await self.dump_snapshot(files_coll)
+        try:
+            inactive_servers = server_coll.find({"bot_in_server": False})
+            inactive_serv = [serv['_id'] async for serv in inactive_servers]
+            files_coll = self.db.files
+            await self.dump_snapshot(files_coll)
 
-        n_docs = await files_coll.count_documents({})
-        res = await files_coll.delete_many({"guild_id": {"$in": inactive_serv}})
-        if res.acknowledged:
-            logger.info(f"Deleted {res.deleted_count} files")
-        else:
-            logger.error(f"Failed to delete files")
-        return res.acknowledged, res.deleted_count != n_docs
+            n_docs = await files_coll.count_documents({})
+            res = await files_coll.delete_many({"guild_id": {"$in": inactive_serv}})
+            if res.acknowledged:
+                logger.info(f"Deleted {res.deleted_count} files")
+            else:
+                logger.error(f"Failed to delete files")
+            return res.acknowledged, res.deleted_count != n_docs
+        except:
+            return False, False
 
 
 async def basic_tests():
