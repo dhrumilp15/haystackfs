18bf4b79247999d60802714885eeeed3807d83ba
William Harrell <wharrell1@protonmail.com>
2021-10-30 10:16:28 -0400

    dealing with timeouts, skipping in template

--- python/export_template.py
+++ python/export_template.py
@@ -2,6 +2,7 @@ from urllib import request
 import os
 
 
+root_dir = "discordfs-export"
 headers = {
     "User-Agent": "discordfs/1.0"
 }
@@ -10,13 +11,23 @@ files = [
 
 ]
 
+if not os.path.exists(root_dir):
+    os.mkdir(root_dir)
+
 for f in files:
+    path = os.path.join(root_dir, f["channel_name"], f["filename"])
+    if os.path.exists(path):
+        f"Skipping {f['url']} (already downloaded)"
+        continue
+    print(f"Fetching {f['url']}")
     req = request.Request(f["url"], {}, headers)
     with request.urlopen(req) as url:
+        if url.status == 429:
+            print("Timeout")
         b = url.read()
-        if not os.path.exists(f["channel_name"]):
-            os.mkdir(f["channel_name"])
-            path = os.path.join(f["channel_name"], f["filename"])
-            with open(path, "wb") as out:
-                out.write(b)
+        if not os.path.exists(os.path.join(root_dir, f["channel_name"])):
+            os.mkdir(os.path.join(root_dir, f["channel_name"]))
+        path = os.path.join(path)
+        with open(path, "wb") as out:
+            out.write(b)
 
