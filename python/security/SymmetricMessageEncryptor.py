c5626e51ae0ed201b0de77d4c1669b542db01c67
Dhrumil Patel <dhrumilp15@gmail.com>
2021-12-01 04:40:50 -0500

    Messae Encryption

---
+++ python/security/SymmetricMessageEncryptor.py
@@ -0,0 +1,43 @@
+"""Generic encryption/decryption service for messages using symmetric encryption."""
+
+
+class SymmetricMessageEncryptor:
+    """Generic encryption/decryption service for messages using symmetric encryption."""
+
+    def __init__(self, algorithm, config=None, key: str = None):
+        """
+        Initialize Message Encryption for symmetric ciphers.
+
+        Args:
+            algorithm: The algorithm with which to encrypt or decrypt
+            key: The key with which to encrypt/decrypt messages
+        """
+        self.config = config
+        self.key = key
+        if 'key' in self.config:
+            self.key = self.config.key
+        self.cipher = algorithm(self.key)
+
+    def encrypt(self, message: str) -> str:
+        """
+        Encrypt a message.
+
+        Args:
+            message - The message to encrypt
+
+        Returns:
+            The encrypted message.
+        """
+        return self.cipher.encrypt(message.encode()).decode()
+
+    def decrypt(self, message: str) -> str:
+        """
+        Decrypt a message.
+
+        Args:
+            message - The message to decrypt
+
+        Returns:
+            The decrypted message.
+        """
+        return self.cipher.decrypt(message).decode()
