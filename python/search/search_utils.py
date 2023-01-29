59d67c018adf7b11eae5c230d3abaff355aa4e4a
Dhrumil Patel <dhrumilp15@gmail.com>
2023-01-28 21:50:05 -0500

    simple_search_engine

---
+++ python/search/search_utils.py
@@ -0,0 +1,37 @@
+from fuzzywuzzy import fuzz
+
+def search_dict_match(metadata, thresh=0.75, **query):
+    for key, value in query.items():
+        if value is None:
+            continue
+        if key == "content" or key == "filename" or key == "custom_filetype":
+            if key == "custom_filetype":
+                key = "filetype"
+            score = fuzz.partial_ratio(value.lower(), metadata[key].lower())
+            if score < thresh:
+                return False
+        elif key == "after":
+            if metadata['created_at'] < value:
+                return False
+        elif key == "before":
+            if metadata['created_at'] > value:
+                return False
+        elif key == "author" or key == "channel":
+            if metadata[key + "_id"] != value.id:
+                return False
+        elif key == "filetype":
+            filetype = metadata['content_type']
+            if filetype is None:
+                value = value[value.index('/') + 1:]
+                filetype = metadata['filetype']
+            if filetype == "jpeg" or filetype == "jpg":
+                filetype = "jpg"
+            if value == "jpeg" or value == "jpg":
+                value = "jpg"
+            if value == 'image' or value == 'audio':
+                if value not in filetype:
+                    return False
+            else:
+                if value != filetype:
+                    return False
+    return True
